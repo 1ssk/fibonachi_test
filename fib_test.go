10d38dbb947d793f562f4f65837c57ab0b33e88d
ssk2 <1sqlroot@gmail.com>
2024-10-15 10:09:22 +0400

    fib_test

--- fib_test.go
+++ fib_test.go
@@ -1,10 +1,33 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFibZero(t *testing.T) {
 	res := fib(0)
 	if res != 0 {
 		t.Fatal("must be 0")
+	}
+}
+
+func TestFibOne(t *testing.T) {
+	res := fib(1)
+	if res != 1 {
+		t.Fatal("must be 1")
+	}
+}
+
+func TestFibNum(t *testing.T) {
+	res := fib(3)
+	if res != 2 {
+		t.Fatal("must be 2")
+	}
+}
+
+func TestFibTwo(t *testing.T) {
+	res := fib(10)
+	if res != 55 {
+		t.Fatal("must be 55")
 	}
 }
